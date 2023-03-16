509d078b0e7a5186ee9c965f07f7182a1e52cd91
Joanna <astronautnemo@gmail.com>
2023-03-16 15:30:53 +0300

    completed GET /spices deliverable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  # Required routes:
+  #   GET /spices: return an array of all spices
+  #   POST /spices: create a new spice
+  #   PATCH /spices/:id: update an existing spice
+  #   DELETE /spices/:id: delete an existing spice
+
+  resources :spices, only: [:index]
 end
